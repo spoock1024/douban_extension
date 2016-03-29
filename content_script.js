cf0b89118a0d975c97722ec5458b6f38068ed95b
spock <wang_monkey1@163.com>
2016-03-29 12:46:16 +0800

    增加了在页面上增加按钮的DOM操作

--- content_script.js
+++ content_script.js
@@ -38,6 +38,48 @@ if(book_info == null){
         msg.isbn = isbn_match[1];
     }
 
+    // cann't write code at here
+    // chrome.tabs.executeScript(null,{code:"document.body.style.backgroundColor='green'"});
+
+    // change the document'e element,this can work
+    // var bodyelement = document.getElementsByTagName("body")[0]
+    // bodyelement.style.backgroundColor = "red"
+
+    // add a button for check book
+    var book_show_div = document.getElementById("interest_sect_level");
+
+    var a_element = document.createElement('a');
+    a_element.className = "j a_show_login colbutt ll";
+    a_element.name = "checkbook";
+    a_element.href = "#";
+    a_element.rel = "nofollow";
+    // var create_a_elemt_str = "<a href='dada'>gdgdg</a>";
+
+    // var create_a_elemt_str = "<input type=\"radio\" name=\"sex\">"
+
+    // var a_element = document.createElement("<input type=\"radio\" name=\"sex\">");
+
+    var span_element = document.createElement("span");
+
+    var form_element = document.createElement("form");
+    form_element.method = "POST";
+    form_element.action = "";
+    form_element.className = "miniform";
+    var input_element = document.createElement("input");
+    input_element.type="submit";
+    input_element.className = "minisubmit j";
+    input_element.value = "检查";
+
+    form_element.appendChild(input_element);
+
+    span_element.appendChild(form_element);
+
+    
+    a_element.appendChild(span_element);
+
+    var star_element = book_show_div.children[3];
+    book_show_div.insertBefore(a_element,star_element);
+
     chrome.runtime.sendMessage(msg);
 }
 
